18b0f7b74004d8493b6fc42b675c4227e8ce4004
malik-evans <malik.evans.dev@outlook.com>
2018-09-30 11:31:43 -0400

    nuxt server integration

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,20 @@
-console.log('server');
+import 'reflect-metadata';
+import * as Express from 'express';
+import config from '../nuxt.config';
+import { Nuxt, Builder } from 'nuxt';
+
+const app= Express();
+const PORT = 8000;
+const nuxt = new Nuxt(config);
+
+app.use(nuxt.render);
+
+function listen(app: Express.Application, port: number  = PORT) {
+    app.listen(PORT, () => {
+        console.log(`App listening on ${PORT}`)
+    })
+}
+
+new Builder(nuxt)
+    .build()
+    .then( _ => listen(app));
