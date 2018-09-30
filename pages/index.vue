c0b6abdc84d7e75fff0742738f0549025d8a3a52
malik-evans <malik.evans.dev@outlook.com>
2018-09-30 10:28:47 -0400

    initial theme integration

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-    <div>Hello from vue</div>
+    <div class="ui card inverted">
+        <div class="content">Hello from semantic</div>
+        <div class="extra">
+            <div class="ui button primary">Button</div>
+        </div>
+    </div>
 </template>
