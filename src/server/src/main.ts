96bb380c32054e1df9d048530eabe907c04142e4
malik-evans <malik.evans.dev@outlook.com>
2018-10-28 12:31:54 -0400

    @nestjs/swagger integration

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,11 +1,19 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {join} from 'path';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const appDocumentOptions = new DocumentBuilder()
+        .setTitle('App')
+        .setBasePath('api')
+        .build();
+    const appDocument = SwaggerModule.createDocument(app, appDocumentOptions);
+    SwaggerModule.setup('swagger.json', app, appDocument);
     app.setGlobalPrefix('api');
     app.useStaticAssets(join(__dirname, '../../client/dist/client'));
+
     await app.listen(8000, () => {
         console.log('listening on port 3000');
     });
