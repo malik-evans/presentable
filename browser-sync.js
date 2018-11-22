1de38c3ef333d85f40e3ef5e7a62a77e768eb310
Malik Evans <33239708+malik-evans@users.noreply.github.com>
2018-11-22 10:38:53 -0500

    fix(browser-sync) watch files

--- browser-sync.js
+++ browser-sync.js
@@ -1,7 +1,8 @@
 module.exports = {
   port: 5000,
   proxy: 'http://localhost:8000/',
-  serve: ['dist/**'],
+  serve: ['dist/browser/**'],
+  files: ["dist/**"],
   reloadDelay: 1000,
   reloadDebounce: 1000,
 };
